658c2f9638dabb468ff99c42ccdd1fca28cd6a83
lunariixx <mgmt@mrrp.dev>
2025-11-02 12:59:05 +0900

    stylize

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-I decided to wipe my old website, as I was not happy with it. I will be working on this over a good while, if you wish to check back later ;D
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { startFPS } from '$lib/scripts/fps';
+
+	onMount(() => {
+		startFPS();
+	});
+</script>
+
+<span id="fps">0</span>
+<div id="wrapper">
+	<div id="overlay"></div>
+	I decided to wipe my old website, as I was not happy with it. I will be working on this over a good
+	while, if you wish to check back later ;D
+	<img
+		src="./images/kitty.jpg"
+		alt="Cat"
+		style="max-width:30%; height:auto; margin-top:20px;"
+	/>
+</div>
