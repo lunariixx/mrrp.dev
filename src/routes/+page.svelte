90802d70544d6f9b582f3c131dbc536cbaf674fb
lunariixx <mgmt@mrrp.dev>
2025-11-06 13:50:16 +0900

    Remove FPS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { startFPS } from '$lib/scripts/fps';
 	import { initSnow } from '$lib/scripts/snow';
 
 	onMount(() => {
-		startFPS();
 		initSnow();
 	});
 </script>
 
-<span id="fps">0</span>
 <div id="wrapper">
 	<div id="overlay"></div>
 	I decided to wipe my old website, as I was not happy with it. I will be working on this over a good
